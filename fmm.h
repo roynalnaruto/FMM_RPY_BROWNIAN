9f12f258b02be5a276ddb10e4854080c7e9fbb01
roynalnaruto <roynalnaruto111@gmail.com>
2014-06-05 18:20:41 -0400

    adding object files

---
+++ fmm.h
@@ -0,0 +1,7 @@
+typedef struct {double dr,di;} complex;
+
+extern void makeone_(int *, int*);
+
+extern void printpos_(int *n, double *source);
+
+extern void computerpy_(int *n, double *source, double *radii, complex *v1, complex *v2, complex *v3, complex *rpy, char* dir);
