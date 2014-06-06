0928f80c4d79b4cdf79651d3d20209262e257e8f
vipulharsh <vipulharsh.iit@gmail.com>
2014-06-06 02:07:59 -0400

    computing local interactions efficiently: done

--- objs/interactions.d
+++
@@ -1,4 +0,0 @@
-/home/vipul/FMM_RPY_BROWNIAN/objs/interactions.o: \
- /home/vipul/FMM_RPY_BROWNIAN/interactions.c \
- /home/vipul/FMM_RPY_BROWNIAN/headers.h \
- /home/vipul/FMM_RPY_BROWNIAN/normal.h /home/vipul/FMM_RPY_BROWNIAN/fmm.h
